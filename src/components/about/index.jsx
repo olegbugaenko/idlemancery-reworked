085664785100336ac922f79d350d24e653fad17b
Oleh <oleh.buhaienko@chatbots.studio>
2024-10-10 22:20:49 +0300

    Fixed issues with actions optimizations

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -7,5 +7,13 @@ export const About = () => {
         <div className={'features'}>
             <p>This is very first playable prototype</p>
         </div>
+        <h3>Idle Awakening v0.0.1.a</h3>
+        <div className={'features'}>
+            <p>Urgent hotfix for console errors players got</p>
+            <p>Fixed action effects lists calculations</p>
+            <p>Upgraded UI for actions lists selection and editing</p>
+            <p>Fixed some text descriptions</p>
+            <p>Fixed bug when primary attrubute didn't affected learning rate</p>
+        </div>
     </div>)
 }
