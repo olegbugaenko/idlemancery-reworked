2d4d731d71bce425aa20844f3861d3fa60e10d3d
Oleh <oleh.buhaienko@chatbots.studio>
2024-10-10 22:23:38 +0300

    Fixed issues with actions optimizations

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -82,7 +82,7 @@ export const Footer = () => {
                     <span>Settings</span>
                 </li>
                 <li className={openedTab === 'about' ? 'active' : ''} onClick={() => openTab('about')}>
-                    <span>v0.0.1 (First Prototype)</span>
+                    <span>v0.0.1.a (Hotfix patch)</span>
                 </li>
             </ul>
         </div>
