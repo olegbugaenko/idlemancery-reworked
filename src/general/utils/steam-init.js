05afd851b66d19e13a3cf4ca0f1c87858ee44bb6
Oleh <oleh.buhaienko@chatbots.studio>
2025-05-20 13:20:33 +0300

    greenworks-sdk

--- src/general/utils/steam-init.js
+++ src/general/utils/steam-init.js
@@ -1,22 +1,17 @@
 // src/general/utils/steam-init.js
 let greenworks = null;
-const fs = require('fs');
 
 function initSteamIfAvailable() {
     if (process.env.IS_STEAM_DEMO === '1' || true) {
         try {
             greenworks = require('../../../native/greenworks');
             if (greenworks.init()) {
-                fs.appendFileSync('log.txt', `Steam API initialized\n`);
                 console.log('Steam API initialized');
                 return true;
             } else {
-                fs.appendFileSync('log.txt', `Steam API failed to initialize\n`);
                 console.warn('Steam API failed to initialize');
             }
         } catch (e) {
-            fs.appendFileSync('log.txt', `Steam API not available: ${e.toString()}\n`);
-            fs.appendFileSync('log.txt', `${e.stack}\n`);
             console.warn('Steam API not available:', e);
         }
     } else {
